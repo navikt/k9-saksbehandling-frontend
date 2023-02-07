bbca96f0ba777be37a1979a764e3e2938f217c14
Hallvard Andreas Stark <hallvard@scel.to>
2023-02-07 13:32:18 +0100

    Kopiert over endring i diagnosekodesøk

--- packages/medisinsk-vilkår/src/ui/components/diagnosekodeoversikt/Diagnosekodeoversikt.tsx
+++ packages/medisinsk-vilkår/src/ui/components/diagnosekodeoversikt/Diagnosekodeoversikt.tsx
@@ -1,9 +1,12 @@
-import { Loader, Modal } from '@navikt/ds-react';
 import { Box, Margin, TitleWithUnderline, WarningIcon } from '@navikt/ft-plattform-komponenter';
 import { get, post } from '@navikt/k9-http-utils';
-import React, { useEffect } from 'react';
-import { useMutation, useQuery } from 'react-query';
+import axios from 'axios';
+import Modal from 'nav-frontend-modal';
+import Spinner from 'nav-frontend-spinner';
+import React, { useMemo } from 'react';
+import { useMutation, useQueries, useQuery } from 'react-query';
 import LinkRel from '../../../constants/LinkRel';
+import Diagnosekode from '../../../types/Diagnosekode';
 import { DiagnosekodeResponse } from '../../../types/DiagnosekodeResponse';
 import { findLinkByRel } from '../../../util/linkUtils';
 import ContainerContext from '../../context/ContainerContext';
@@ -12,6 +15,13 @@ import DiagnosekodeModal from '../diagnosekode-modal/DiagnosekodeModal';
 import Diagnosekodeliste from '../diagnosekodeliste/Diagnosekodeliste';
 import IconWithText from '../icon-with-text/IconWithText';
 import WriteAccessBoundContent from '../write-access-bound-content/WriteAccessBoundContent';
+
+Modal.setAppElement('#app');
+
+const fetchDiagnosekoderByQuery = (queryString: string): Promise<Diagnosekode> =>
+    axios
+        .get(`/k9/diagnosekoder?query=${queryString}&max=8`)
+        .then((response) => (response.data && response.data.length === 1 ? response.data[0] : { kode: queryString }));
 
 interface DiagnosekodeoversiktProps {
     onDiagnosekoderUpdated: () => void;
@@ -22,10 +32,6 @@ const Diagnosekodeoversikt = ({ onDiagnosekoderUpdated }: DiagnosekodeoversiktPr
     const [modalIsOpen, setModalIsOpen] = React.useState(false);
     const addButtonRef = React.useRef<HTMLButtonElement>();
 
-    useEffect(() => {
-        Modal.setAppElement(document.body);
-    }, []);
-
     const hentDiagnosekoder = () =>
         get<DiagnosekodeResponse>(endpoints.diagnosekoder, httpErrorHandler).then(
             (response: DiagnosekodeResponse) => response
@@ -35,6 +41,19 @@ const Diagnosekodeoversikt = ({ onDiagnosekoderUpdated }: DiagnosekodeoversiktPr
 
     const { diagnosekoder, links, behandlingUuid, versjon } = data;
     const endreDiagnosekoderLink = findLinkByRel(LinkRel.ENDRE_DIAGNOSEKODER, links);
+
+    const diagnosekoderMedNavnResponses = useQueries(
+        diagnosekoder.map((diagnosekode) => ({
+            queryKey: ['diagnosekode', diagnosekode],
+            queryFn: () => fetchDiagnosekoderByQuery(diagnosekode),
+            refetchOnWindowFocus: false,
+        }))
+    );
+
+    const diagnosekoderMedNavn = useMemo(
+        () => diagnosekoderMedNavnResponses.filter((response) => !!response.data).map((response) => response.data),
+        [diagnosekoderMedNavnResponses]
+    );
 
     const focusAddButton = () => {
         if (addButtonRef.current) {
@@ -102,7 +121,7 @@ const Diagnosekodeoversikt = ({ onDiagnosekoderUpdated }: DiagnosekodeoversiktPr
                 Diagnosekoder
             </TitleWithUnderline>
             {isLoading ? (
-                <Loader size="large" />
+                <Spinner />
             ) : (
                 <Box marginTop={Margin.large}>
                     {diagnosekoder.length === 0 && (
@@ -110,7 +129,7 @@ const Diagnosekodeoversikt = ({ onDiagnosekoderUpdated }: DiagnosekodeoversiktPr
                     )}
                     {diagnosekoder.length >= 1 && (
                         <Diagnosekodeliste
-                            diagnosekoder={diagnosekoder}
+                            diagnosekoder={diagnosekoderMedNavn}
                             onDeleteClick={slettDiagnosekodeMutation.mutate}
                         />
                     )}
--- packages/medisinsk-vilkår/src/ui/form/pure/PureDiagnosekodeSelector.tsx
+++ packages/medisinsk-vilkår/src/ui/form/pure/PureDiagnosekodeSelector.tsx
@@ -1,9 +1,15 @@
-import { BodyShort, Label } from '@navikt/ds-react';
+import { BodyShort } from '@navikt/ds-react';
 import { Autocomplete, FieldError } from '@navikt/ft-plattform-komponenter';
+import { Label } from 'nav-frontend-skjema';
 import * as React from 'react';
 import Diagnosekode from '../../../types/Diagnosekode';
 import DeleteButton from '../../components/delete-button/DeleteButton';
 import styles from './diagnosekodeSelector.css';
+
+interface Suggestion {
+    key: string;
+    value: string;
+}
 
 interface DiagnosekodeSelectorProps {
     label: string;
@@ -16,7 +22,7 @@ interface DiagnosekodeSelectorProps {
 }
 
 const fetchDiagnosekoderByQuery = (queryString: string) =>
-    fetch(`/k9/diagnosekoder/?query=${queryString}&max=8`).then((response) => response.json());
+    fetch(`/k9/diagnosekoder?query=${queryString}&max=8`).then((response) => response.json());
 
 const PureDiagnosekodeSelector = ({
     label,
@@ -30,6 +36,7 @@ const PureDiagnosekodeSelector = ({
     const [suggestions, setSuggestions] = React.useState([]);
     const [inputValue, setInputValue] = React.useState('');
     const [isLoading, setIsLoading] = React.useState(false);
+    const [selectedDiagnosekoderFullName, setSelectedDiagnosekoderFullName] = React.useState<Suggestion[]>([]);
 
     const getUpdatedSuggestions = async (queryString: string) => {
         if (queryString.length >= 3) {
@@ -59,15 +66,13 @@ const PureDiagnosekodeSelector = ({
         setSuggestions(newSuggestionList);
     };
 
-    const removeDiagnosekode = (diagnosekodeToRemove: string) =>
+    const removeDiagnosekode = (diagnosekodeToRemove: string) => {
         onChange(selectedDiagnosekoder.filter((selectedDiagnosekode) => selectedDiagnosekode !== diagnosekodeToRemove));
-
+    };
     return (
         <div className={styles.diagnosekodeContainer}>
             <div className={hideLabel ? styles.diagnosekodeContainer__hideLabel : ''}>
-                <Label as="label" htmlFor={name}>
-                    {label}
-                </Label>
+                <Label htmlFor={name}>{label}</Label>
             </div>
             <div className={styles.diagnosekodeContainer__autocompleteContainer}>
                 <Autocomplete
@@ -82,6 +87,7 @@ const PureDiagnosekodeSelector = ({
                         setInputValue('');
                         if (!selectedDiagnosekoder.includes(e.key)) {
                             onChange([...selectedDiagnosekoder, e.key]);
+                            setSelectedDiagnosekoderFullName([...selectedDiagnosekoderFullName, e]);
                         }
                     }}
                     ariaLabel="Søk etter diagnose"
@@ -93,12 +99,18 @@ const PureDiagnosekodeSelector = ({
             {errorMessage && <FieldError message={errorMessage} />}
             {selectedDiagnosekoder.length > 0 && (
                 <ul className={styles.diagnosekodeContainer__diagnosekodeList}>
-                    {selectedDiagnosekoder.map((selectedDiagnosekode) => (
-                        <li key={selectedDiagnosekode}>
-                            <BodyShort size="small">{selectedDiagnosekode}</BodyShort>
-                            <DeleteButton onClick={() => removeDiagnosekode(selectedDiagnosekode)} />
-                        </li>
-                    ))}
+                    {selectedDiagnosekoder.map((selectedDiagnosekode) => {
+                        const fullName = selectedDiagnosekoderFullName.find(
+                            (selectedDiagnosekodeFullName) => selectedDiagnosekodeFullName.key === selectedDiagnosekode
+                        );
+                        const diagnosekodeLabel = fullName ? fullName.value : selectedDiagnosekode;
+                        return (
+                            <li key={selectedDiagnosekode}>
+                                <BodyShort size="small">{diagnosekodeLabel}</BodyShort>
+                                <DeleteButton onClick={() => removeDiagnosekode(selectedDiagnosekode)} />
+                            </li>
+                        );
+                    })}
                 </ul>
             )}
         </div>
