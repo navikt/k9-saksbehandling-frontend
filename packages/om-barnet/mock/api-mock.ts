80d694f03dec9102072b303dc75ac4c726b7d38e
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2023-02-10 07:47:06 +0100

    msw som mock (#21)
    fjernet nav-frontend-pakker

--- packages/om-barnet/mock/api-mock.ts
+++ packages/om-barnet/mock/api-mock.ts
@@ -1,28 +1,25 @@
-import cors from 'cors';
-import express from 'express';
+import { rest } from 'msw';
 
-const app = express();
+export const handlers = [
+    rest.get('http://localhost:8082/mock/rettVedDod', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                vurdering: 'dette er en vurdering',
+                rettVedDødType: 'RETT_12_UKER',
+            })
+        );
+    }),
 
-app.use(express.json());
-
-app.use(
-    cors({
-        origin: 'http://localhost:8081',
-    })
-);
-
-app.use('/mock/rettVedDod', (req, res) => {
-    res.send({
-        vurdering: 'dette er en vurdering',
-        rettVedDødType: 'RETT_12_UKER',
-    });
-});
-
-app.use('/mock/omPleietrengende', (req, res) => {
-    res.send({ fnr: '012345678912', navn: 'DUCK DOLE', diagnosekoder: ['R619', 'A300'], dodsdato: '2021-05-26' });
-});
-
-const port = 8082;
-app.listen(port, () => {
-    console.log('API-mock listening on port', port);
-});
+    rest.get('http://localhost:8082/mock/omPleietrengende', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                fnr: '012345678912',
+                navn: 'DUCK DOLE',
+                diagnosekoder: ['R619', 'A300'],
+                dodsdato: '2021-05-26',
+            })
+        );
+    }),
+];
