5bc5e868aa711b9f368f35734a40aaf102566e77
Hallvard Andreas Stark <hallvard@scel.to>
2023-02-22 13:43:54 +0100

    flyttet over omsorgsdager frontend

---
+++ packages/omsorgsdager-1/src/types/VilkarKroniskSyktBarnProps.ts
@@ -0,0 +1,21 @@
+import { FormState } from './FormState';
+import { InformasjonOmVurdertVilkar } from './InformasjonOmVurdertVilkar';
+
+export interface VilkarKroniskSyktBarnProps {
+    behandlingsID: string;
+    vedtakFattetVilkarOppfylt: boolean;
+    soknadsdato: string;
+    aksjonspunktLost: boolean;
+    informasjonOmVilkar?: InformasjonOmVurdertVilkar;
+    lesemodus: boolean;
+    informasjonTilLesemodus?: InformasjonTilLesemodusKroniskSyk;
+    losAksjonspunkt: (endreHarDokumentasjonOgFravaerRisiko, begrunnelse, avslagsKode, fraDato) => void;
+    formState: FormState;
+}
+
+export interface InformasjonTilLesemodusKroniskSyk {
+    begrunnelse: string;
+    vilkarOppfylt: boolean;
+    avslagsArsakErIkkeRiskioFraFravaer: boolean;
+    fraDato: string;
+}
