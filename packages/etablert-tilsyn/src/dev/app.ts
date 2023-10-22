6acdd18de3fc1358e9b399a90cae6dc9b4c5e381
Hallvard Andreas Stark <hallvard@scel.to>
2023-10-22 17:51:44 +0200

    Pakkifisert etablert tilsyn

--- packages/etablert-tilsyn/src/dev/app.ts
+++ packages/etablert-tilsyn/src/dev/app.ts
@@ -1,4 +1,6 @@
-import renderers from './util/renderers';
+import renderers from '../util/renderers';
+import '@navikt/ft-plattform-komponenter/dist/style.css';
+import '@navikt/ds-css';
 
 (window as any).renderTilsynApp = async (appId, data) => {
     const { renderAppInSuccessfulState } = renderers;
