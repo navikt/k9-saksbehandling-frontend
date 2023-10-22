c62f193df49391692bc1997ebfd5362a8b217cfd
Hallvard Andreas Stark <hallvard@scel.to>
2023-10-22 21:25:27 +0200

    fjerner deploy-relaterte filer

--- packages/medisinsk-vilkår/majorVersion.js
+++
@@ -1,5 +0,0 @@
-const pkg = require('./package.json');
-const version = pkg.version;
-const versionSegments = version.split('.');
-const majorVersion = versionSegments[0];
-console.log(majorVersion);
