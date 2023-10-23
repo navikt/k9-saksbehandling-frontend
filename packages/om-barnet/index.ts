2d2442d0cc56e6e344034e2aca3b4b1206f44f28
Hallvard Andreas Stark <hallvard@scel.to>
2023-10-23 13:13:45 +0200

    Fikser feil i eksport

--- packages/om-barnet/index.ts
+++ packages/om-barnet/index.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export { default as MedisinskVilkår } from './src/ui/MainComponent';
+export { default as OmBarnet } from './src/ui/MainComponent';
